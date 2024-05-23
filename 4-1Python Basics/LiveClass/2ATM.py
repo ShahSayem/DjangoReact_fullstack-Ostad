0e707774d5a99267930afc9681755f3951107de1
Shah Sayem <shahsayemahmad@gmail.com>
2024-05-24 02:40:00 +0600

    Add Error handling feature

--- 4-1Python Basics/LiveClass/2ATM.py
+++ 4-1Python Basics/LiveClass/2ATM.py
@@ -14,32 +14,48 @@ def atm():
             print("4. Change PIN")
             print("5. Exit\n")
 
-            choice = int(input("Enter your option: "))
+            choice = input("Enter your option: ")
 
-            if (choice == 1):
+            if (choice == "1"):
                 print(f"Your balance: ${balance}")
-            elif (choice == 2):
-                amount = float(input("Enter the amount to withdraw: $"))
 
-                if (amount > balance):
-                    print("Insufficient balance!!!\n")
-                else:
-                    balance -= amount
-                    print(f"${amount} has been withdrawn. Remaining balance: ${balance}\n")
-            elif (choice == 3):
-                amount = float(input("Enter the amount to deposit: $"))  
-                balance += amount
-                print(f"${amount} has been deposited. New balance: ${balance}\n")
-            elif (choice == 4):
+            elif (choice == "2"):
+                amount = input("Enter the amount to withdraw: $")
+
+                try:
+                    amount = float(amount)
+
+                    if (amount > balance):
+                        print("Insufficient balance!!!\n")
+                    else:
+                        balance -= amount
+                        print(f"${amount} has been withdrawn. Remaining balance: ${balance}\n")
+                except ValueError:
+                    print("Invalid amount format (-_-). Amount must be in numeric format.\n")
+
+            elif (choice == "3"):
+                amount = input("Enter the amount to deposit: $")
+
+                try:
+                    amount = float(amount)
+                    balance += amount
+                    print(f"${amount} has been deposited. New balance: ${balance}\n")
+                except ValueError:
+                    print("Invalid amount format (-_-). Amount must be in numeric format.\n")
+                    
+            elif (choice == "4"):
                 newPIN = input("Enter your new 4 digit PIN:")
                 if (len(newPIN) == 4 and newPIN.isdigit()):
                     pin = enteredPIN = newPIN
                     print("PIN changed successfully!\n")
                 else:
                     print("Invalid PIN format (-_-). PIN must be 4 digit number.\n")
-            elif (choice == 5):
+            elif (choice == "5"):
                 print("Thanks for banking with us. \nHave a nice day!\n")
                 break
+            else:
+                print("Invalid opotion format (-_-). Option must be a single digit number from 1 to 5.\n")
+
         elif(enteredPIN.lower() == "exit"):
             print("Exit successfully. \nHave a nice day!\n")
             break
