2e9838b70afbd5e08a0a799cb0162b992b9b8bba
Shah Sayem <shahsayemahmad@gmail.com>
2024-06-24 01:04:33 +0600

    Update OTP Generator

--- 0Projects/0-OTP Generator/JS/app.js
+++ 0Projects/0-OTP Generator/JS/app.js
@@ -1,6 +1,8 @@
 const switchBtn = document.getElementById("checkbox");
-                                                //switch-btn-toggler
-//Event handler
+const generatedOTP = document.getElementById("generate-otp-codeID");
+const otpInputList = document.getElementById("otp-input-listID");
+                                                
+//Event handler - switch-btn-toggler
 switchBtn.addEventListener("click", function (){
     let root = document.querySelector(":root");
     let rootStyle = getComputedStyle(root);
@@ -13,3 +15,28 @@ switchBtn.addEventListener("click", function (){
         root.style.setProperty("--theme-bg-white", "#e0e0e0");
     }
 });
+
+
+function displayOTP(){
+    generatedOTP.innerHTML = `
+        <div>OTP code is</div>
+        <button class="reset-btn">reset</button>
+        <div>expire in 0s</div>
+    `;
+}
+
+setTimeout(displayOTP, 1000);
+
+const checkOTP = (elem) => {
+    const currElem = elem.target;
+    const currVal = elem.target.value;
+
+    const nextChild = currElem.nextElementSibling;
+
+    //Forword to next
+    if (nextChild){
+        nextChild.focus();
+    }
+};
+
+otpInputList.addEventListener("input", checkOTP);
